ea853adb19a6813b125f6e01358723590040272c
Shan Gao <shawn.chris.gao@outlook.com>
2016-04-25 10:14:19 -0500

    04/25/2016

--- other/Rectangle.java
+++ other/Rectangle.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Rectangle {
 	public static boolean overlap(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2) {
         int left = Math.max(x1, x2);
-        int right = Math.max(Math.min(x1 + w1, x2 + w2), left);
-        int bottom = Math.max(y1, y2), top = Math.max(Math.min(y1 + h1, y2 + h2), bottom);
-        return (right >=left) && (top >= bottom);
+        int right = Math.min(x1 + w1, x2 + w2);
+        int bottom = Math.max(y1, y2);
+        int top = Math.min(y1 + h1, y2 + h2);
+        return (right >= left) && (top >= bottom);
     }
 	
 	public static void main(String[] args) {
