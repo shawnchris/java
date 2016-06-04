b0d7273c5c57329d4025458402514952f3fa0718
Shawn Gao <shawn.chris.gao@outlook.com>
2016-06-03 23:25:08 -0500

    BellmanFord

---
+++ csc421/BellmanFord.java
@@ -0,0 +1,620 @@
+package csc421;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Bellman Ford Shortest Path
+ * @author Shan Gao
+ *
+ */
+public class BellmanFord {
+    private double[] distTo;               // distTo[v] = distance  of shortest s->v path
+    private DirectedEdge[] edgeTo;         // edgeTo[v] = last edge on shortest s->v path
+    private boolean[] onQueue;             // onQueue[v] = is v currently on the queue?
+    private Queue<Integer> queue;          // queue of vertices to relax
+    private int cost;                      // number of calls to relax()
+    private Iterable<DirectedEdge> cycle;  // negative cycle (or null if no such cycle)
+    
+    public static void main(String[] args) {
+    	
+    	EdgeWeightedDigraph G = new EdgeWeightedDigraph("BellmanFordinput");
+    	int s = G.S();
+
+        BellmanFord sp = new BellmanFord(G, s);
+
+        // print negative cycle
+        if (sp.hasNegativeCycle()) {
+        	System.out.println("NegativeCycle found!");
+            for (DirectedEdge e : sp.negativeCycle())
+                System.out.println(e);
+        }
+
+        // print shortest paths
+        else {
+            for (int v = 0; v < G.V(); v++) {
+                if (sp.hasPathTo(v)) {
+                    System.out.printf("%d to %d (%5.2f)  ", s, v, sp.distTo(v));
+                    for (DirectedEdge e : sp.pathTo(v)) {
+                        System.out.print(e + "   ");
+                    }
+                    System.out.println();
+                }
+                else {
+                    System.out.printf("%d to %d           no path\n", s, v);
+                }
+            }
+        }
+    }
+
+    public BellmanFord(EdgeWeightedDigraph G, int s) {
+        distTo  = new double[G.V()];
+        edgeTo  = new DirectedEdge[G.V()];
+        onQueue = new boolean[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY;
+        distTo[s] = 0.0;
+
+        // Bellman-Ford algorithm
+        queue = new Queue<Integer>();
+        queue.enqueue(s);
+        onQueue[s] = true;
+        while (!queue.isEmpty() && !hasNegativeCycle()) {
+            int v = queue.dequeue();
+            onQueue[v] = false;
+            relax(G, v);
+        }
+
+        assert check(G, s);
+    }
+
+    // relax vertex v and put other endpoints on queue if changed
+    private void relax(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e : G.adj(v)) {
+            int w = e.to();
+            if (distTo[w] > distTo[v] + e.weight()) {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+                if (!onQueue[w]) {
+                    queue.enqueue(w);
+                    onQueue[w] = true;
+                }
+            }
+            if (cost++ % G.V() == 0) {
+                findNegativeCycle();
+                if (hasNegativeCycle()) return;  // found a negative cycle
+            }
+        }
+    }
+
+    public boolean hasNegativeCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<DirectedEdge> negativeCycle() {
+        return cycle;
+    }
+
+    // by finding a cycle in predecessor graph
+    private void findNegativeCycle() {
+        int V = edgeTo.length;
+        EdgeWeightedDigraph spt = new EdgeWeightedDigraph(V);
+        for (int v = 0; v < V; v++)
+            if (edgeTo[v] != null)
+                spt.addEdge(edgeTo[v]);
+
+        EdgeWeightedDirectedCycle finder = new EdgeWeightedDirectedCycle(spt);
+        cycle = finder.cycle();
+    }
+
+    public double distTo(int v) {
+        if (hasNegativeCycle())
+            throw new UnsupportedOperationException("Negative cost cycle exists");
+        return distTo[v];
+    }
+
+    public boolean hasPathTo(int v) {
+        return distTo[v] < Double.POSITIVE_INFINITY;
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+        if (hasNegativeCycle())
+            throw new UnsupportedOperationException("Negative cost cycle exists");
+        if (!hasPathTo(v)) return null;
+        Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+            path.push(e);
+        }
+        return path;
+    }
+
+    private boolean check(EdgeWeightedDigraph G, int s) {
+
+        // has a negative cycle
+        if (hasNegativeCycle()) {
+            double weight = 0.0;
+            for (DirectedEdge e : negativeCycle()) {
+                weight += e.weight();
+            }
+            if (weight >= 0.0) {
+                System.err.println("error: weight of negative cycle = " + weight);
+                return false;
+            }
+        }
+
+        // no negative cycle reachable from source
+        else {
+
+            // check that distTo[v] and edgeTo[v] are consistent
+            if (distTo[s] != 0.0 || edgeTo[s] != null) {
+                System.err.println("distanceTo[s] and edgeTo[s] inconsistent");
+                return false;
+            }
+            for (int v = 0; v < G.V(); v++) {
+                if (v == s) continue;
+                if (edgeTo[v] == null && distTo[v] != Double.POSITIVE_INFINITY) {
+                    System.err.println("distTo[] and edgeTo[] inconsistent");
+                    return false;
+                }
+            }
+
+            // check that all edges e = v->w satisfy distTo[w] <= distTo[v] + e.weight()
+            for (int v = 0; v < G.V(); v++) {
+                for (DirectedEdge e : G.adj(v)) {
+                    int w = e.to();
+                    if (distTo[v] + e.weight() < distTo[w]) {
+                        System.err.println("edge " + e + " not relaxed");
+                        return false;
+                    }
+                }
+            }
+
+            // check that all edges e = v->w on SPT satisfy distTo[w] == distTo[v] + e.weight()
+            for (int w = 0; w < G.V(); w++) {
+                if (edgeTo[w] == null) continue;
+                DirectedEdge e = edgeTo[w];
+                int v = e.from();
+                if (w != e.to()) return false;
+                if (distTo[v] + e.weight() != distTo[w]) {
+                    System.err.println("edge " + e + " on shortest path not tight");
+                    return false;
+                }
+            }
+        }
+
+        System.out.println("Satisfies optimality conditions");
+        System.out.println();
+        return true;
+    }
+}
+
+class EdgeWeightedDigraph {
+    private int V;
+    private int E;
+    private Bag<DirectedEdge>[] adj;
+    private int S = 0;
+    
+    public EdgeWeightedDigraph(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<DirectedEdge>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<DirectedEdge>();
+    }
+    
+    public EdgeWeightedDigraph(String filename) {
+    	try {
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+			int linecounter = 0;
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				linecounter++;
+				String[] split = line.trim().split("\\s+");
+				if (linecounter==1) {
+					this.V = Integer.parseInt(split[0]);
+			        this.E = 0;
+			        adj = (Bag<DirectedEdge>[]) new Bag[V];
+			        for (int v = 0; v < V; v++)
+			            adj[v] = new Bag<DirectedEdge>();
+				}
+				else if (linecounter==2) {
+					this.S = Integer.parseInt(split[0]);
+				}
+				else {
+					int u = linecounter - 3;
+					for (int v = 0; v < V; v++) {
+						int w = Integer.parseInt(split[v]);
+						if (w!=0) {
+							addEdge(new DirectedEdge(u, v, w));
+							//System.out.println("Added edge: " + u + "->" + v + " " + w);
+						}
+					}
+				}
+			};
+			br.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+    
+    public int S() {
+        return S;
+    }
+
+    // throw an IndexOutOfBoundsException unless 0 <= v < V
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    public void addEdge(DirectedEdge e) {
+        int v = e.from();
+        int w = e.to();
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(e);
+        E++;
+    }
+
+    public Iterable<DirectedEdge> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    public int outdegree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+    public Iterable<DirectedEdge> edges() {
+        Bag<DirectedEdge> list = new Bag<DirectedEdge>();
+        for (int v = 0; v < V; v++) {
+            for (DirectedEdge e : adj(v)) {
+                list.add(e);
+            }
+        }
+        return list;
+    } 
+
+    public String toString() {
+        String NEWLINE = System.getProperty("line.separator");
+        StringBuilder s = new StringBuilder();
+        s.append(V + " " + E + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (DirectedEdge e : adj[v]) {
+                s.append(e + "  ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+}
+
+class Bag<Item> implements Iterable<Item> {
+    private int N;               // number of elements in bag
+    private Node<Item> first;    // beginning of bag
+
+    // helper linked list class
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+
+    public Bag() {
+        first = null;
+        N = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void add(Item item) {
+        Node<Item> oldfirst = first;
+        first = new Node<Item>();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+    public Iterator<Item> iterator()  {
+        return new ListIterator<Item>(first);  
+    }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next; 
+            return item;
+        }
+    }
+}
+
+class DirectedEdge { 
+    private final int v;
+    private final int w;
+    private final double weight;
+
+    public DirectedEdge(int v, int w, double weight) {
+        if (v < 0) throw new IndexOutOfBoundsException("Vertex names must be nonnegative integers");
+        if (w < 0) throw new IndexOutOfBoundsException("Vertex names must be nonnegative integers");
+        if (Double.isNaN(weight)) throw new IllegalArgumentException("Weight is NaN");
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    public String toString() {
+        return v + "->" + w + " " + String.format("%5.2f", weight);
+    }
+}
+
+class Queue<Item> implements Iterable<Item> {
+    private int N;               // number of elements on queue
+    private Node<Item> first;    // beginning of queue
+    private Node<Item> last;     // end of queue
+
+    // helper linked list class
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+
+    public Queue() {
+        first = null;
+        last  = null;
+        N = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;     
+    }
+
+    public Item peek() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        return first.item;
+    }
+
+    public void enqueue(Item item) {
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else           oldlast.next = last;
+        N++;
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        Item item = first.item;
+        first = first.next;
+        N--;
+        if (isEmpty()) last = null;   // to avoid loitering
+        return item;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this)
+            s.append(item + " ");
+        return s.toString();
+    } 
+
+    public Iterator<Item> iterator()  {
+        return new ListIterator<Item>(first);  
+    }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next; 
+            return item;
+        }
+    }
+}
+
+class Stack<Item> implements Iterable<Item> {
+    private int N;          // size of the stack
+    private Node first;     // top of stack
+
+    // helper linked list class
+    private class Node {
+        private Item item;
+        private Node next;
+    }
+
+    public Stack() {
+        first = null;
+        N = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+    public Item pop() {
+        if (isEmpty()) throw new RuntimeException("Stack underflow");
+        Item item = first.item;        // save item to return
+        first = first.next;            // delete first node
+        N--;
+        return item;                   // return the saved item
+    }
+
+    public Item peek() {
+        if (isEmpty()) throw new RuntimeException("Stack underflow");
+        return first.item;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this)
+            s.append(item + " ");
+        return s.toString();
+    }
+
+    public Iterator<Item> iterator()  { return new ListIterator();  }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next; 
+            return item;
+        }
+    }
+}
+
+class EdgeWeightedDirectedCycle {
+    private boolean[] marked;             // marked[v] = has vertex v been marked?
+    private DirectedEdge[] edgeTo;        // edgeTo[v] = previous edge on path to v
+    private boolean[] onStack;            // onStack[v] = is vertex on the stack?
+    private Stack<DirectedEdge> cycle;    // directed cycle (or null if no such cycle)
+
+    public EdgeWeightedDirectedCycle(EdgeWeightedDigraph G) {
+        marked  = new boolean[G.V()];
+        onStack = new boolean[G.V()];
+        edgeTo  = new DirectedEdge[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            if (!marked[v]) dfs(G, v);
+
+        // check that digraph has a cycle
+        assert check(G);
+    }
+
+    // check that algorithm computes either the topological order or finds a directed cycle
+    private void dfs(EdgeWeightedDigraph G, int v) {
+        onStack[v] = true;
+        marked[v] = true;
+        for (DirectedEdge e : G.adj(v)) {
+            int w = e.to();
+
+            // short circuit if directed cycle found
+            if (cycle != null) return;
+
+            //found new vertex, so recur
+            else if (!marked[w]) {
+                edgeTo[w] = e;
+                dfs(G, w);
+            }
+
+            // trace back directed cycle
+            else if (onStack[w]) {
+                cycle = new Stack<DirectedEdge>();
+                while (e.from() != w) {
+                    cycle.push(e);
+                    e = edgeTo[e.from()];
+                }
+                cycle.push(e);
+            }
+        }
+
+        onStack[v] = false;
+    }
+
+    public boolean hasCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<DirectedEdge> cycle() {
+        return cycle;
+    }
+
+    // certify that digraph is either acyclic or has a directed cycle
+    private boolean check(EdgeWeightedDigraph G) {
+
+        // edge-weighted digraph is cyclic
+        if (hasCycle()) {
+            // verify cycle
+            DirectedEdge first = null, last = null;
+            for (DirectedEdge e : cycle()) {
+                if (first == null) first = e;
+                if (last != null) {
+                    if (last.to() != e.from()) {
+                        System.err.printf("cycle edges %s and %s not incident\n", last, e);
+                        return false;
+                    }
+                }
+                last = e;
+            }
+
+            if (last.to() != first.from()) {
+                System.err.printf("cycle edges %s and %s not incident\n", last, first);
+                return false;
+            }
+        }
+        return true;
+    }
+}
