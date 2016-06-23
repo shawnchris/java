3f10abc63509361da3e95265959b684453c496e2
Shawn Gao <shawn.chris.gao@outlook.com>
2016-06-23 15:34:16 -0500

    updated

--- leetcode/A099_Recover_Binary_Search_Tree.java
+++ leetcode/A099_Recover_Binary_Search_Tree.java
@@ -1,36 +1,35 @@
 package leetcode;
-
+/* Two possibilities: Node 1 and Node 2 are adjacent; are not.
+ * 1. 1, 2, 3, 4, 5, 6 -> 1, 3, 2, 4, 5, 6
+ * 2. 1, 2, 3, 4, 5, 6 -> 1, 5, 3, 4, 2, 6
+*/
 public class A099_Recover_Binary_Search_Tree {
-	TreeNode firstElement = null;
-    TreeNode secondElement = null;
-    TreeNode prevElement = new TreeNode(Integer.MIN_VALUE);
+	// Two pointers to record wrong nodes
+    TreeNode first = null, second = null;
+    // Pointer to record previously traversed node
+    TreeNode previous = new TreeNode(Integer.MIN_VALUE);
 
     public void recoverTree(TreeNode root) {
-
-        // In order traversal to find the two elements
+    	// Traverse the tree IN-ORDER to find the wrong nodes
         traverse(root);
-
-        // Swap the values of the two nodes
-        int temp = firstElement.val;
-        firstElement.val = secondElement.val;
-        secondElement.val = temp;
+        
+        // Swap values of the wrong nodes
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
     }
 
     private void traverse(TreeNode root) {
-
-        if (root == null)
-            return;
-
+    	if (root == null) return;
+        
         traverse(root.left);
         
-        if (prevElement.val > root.val) {
-            if (firstElement == null)
-                firstElement = prevElement;
-            if (firstElement != null)
-                secondElement = root;
-        }        
-        prevElement = root;
-
+        if (previous.val > root.val) { // Find wrong node
+            if (first == null) first = previous;
+            if (first != null) second = root;
+        }
+        previous = root;
+        
         traverse(root.right);
     }
 }
