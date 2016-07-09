75082028fbb49ced83ce73f5ad7a10d1763742ae
Shawn Gao <shawn.chris.gao@outlook.com>
2016-07-09 07:54:14 -0500

    netease interview question

---
+++ other/HelloB.java
@@ -0,0 +1,32 @@
+package other;
+
+public class HelloB extends HelloA {
+	public HelloB() {
+	}
+
+	{
+		System.out.println("I’m B class");
+	}
+	static {
+		System.out.println("static B");
+	}
+
+	public static void main(String[] args) {
+		new HelloB();
+		int i = 0;
+		i = i++;
+		System.out.println(i);
+	}
+}
+
+class HelloA {
+	public HelloA() {
+	}
+
+	{
+		System.out.println("I’m A class");
+	}
+	static {
+		System.out.println("static A");
+	}
+}
