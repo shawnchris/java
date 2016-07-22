b9aaaea5a707896dd383d61ed413c54363830d29
shawnchris <shawn.chris.gao@outlook.com>
2016-07-22 08:55:46 -0500

    leetcode 42

--- leetcode/A042_Trapping_Rain_Water.java
+++ leetcode/A042_Trapping_Rain_Water.java
@@ -1,5 +1,26 @@
 package leetcode;
 
 public class A042_Trapping_Rain_Water {
-
+	public int trap(int[] height) {
+        int len = height.length;
+        if (len <= 2) return 0;
+        
+        int sum = 0;
+        int s = 0, e = len - 1, top = 0;
+        
+        while (s < e) {
+            if (height[s] < height[e]) {
+                top = Math.max(top, height[s]);
+                sum += top - height[s];
+                s++;
+            }
+            else {
+                top = Math.max(top, height[e]);
+                sum += top - height[e];
+                e--;
+            }
+        }
+        
+        return sum;
+    }
 }
