d8828fa6c4fa2a393bd2e562bef212953096bf57
shawnchris <shawn.chris.gao@outlook.com>
2016-07-21 20:00:54 -0500

    leetcode 44

--- leetcode/A044_Wildcard_Matching.java
+++ leetcode/A044_Wildcard_Matching.java
@@ -1,5 +1,38 @@
 package leetcode;
 
 public class A044_Wildcard_Matching {
-
+    public boolean isMatch(String s, String p) {
+        int slen = s.length();
+		int plen = p.length();
+		int sp=0, pp=0, matchp=0, starp=-1;
+		
+		while (sp<slen) {
+		    // advancing both pointers
+			if (pp<plen && (s.charAt(sp)==p.charAt(pp) || p.charAt(pp)=='?')) {
+				sp++; pp++;
+			}
+			// * found, only advancing pattern pointer
+			else if (pp<plen && p.charAt(pp)=='*') {
+				starp=pp;
+				matchp=sp;
+				//sp++;
+				pp++;
+			}
+			// last pattern pointer was *, advancing string pointer
+			else if(starp!=-1) {
+				pp = starp+1;
+				matchp++;
+				sp=matchp;
+			}
+			// current pattern pointer is not star, last pattern pointer was not *
+            // characters do not match
+			else
+				return false;
+		}
+		// check for remaining characters in pattern
+		while (pp<plen && p.charAt(pp)=='*')
+			pp++;
+		
+		return (pp==plen);
+    }
 }
