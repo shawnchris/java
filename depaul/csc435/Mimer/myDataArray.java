454363700629e81879f4e0b7f59ffb4f97b0436e
shawnchris <shawn.chris.gao@outlook.com>
2016-07-30 17:22:16 -0500

    Mimer

---
+++ depaul/csc435/Mimer/myDataArray.java
@@ -0,0 +1,9 @@
+package depaul.csc435.Mimer;
+
+import java.util.ArrayList;
+
+public class myDataArray {
+	public int num_lines = 0;
+	// I modified the data structure to use ArrayList. So don't need to worry about overflow.
+	public ArrayList<String> lines = new ArrayList<String>();
+}
