63b2cb2b26620bd797ccde7c9102bfd075eb24b2
shawnchris <shawn.chris.gao@outlook.com>
2016-09-07 17:55:41 -0500

    258

---
+++ leetcode/A258_Add_Digits.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class A258_Add_Digits {
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return (num % 9);
+    }
+}
