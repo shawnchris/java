66eab7a23fd41122cc900a8d81518e2fe7cd23e0
shawnchris <shawn.chris.gao@outlook.com>
2016-11-16 12:58:01 -0600

    243

---
+++ leetcode/A243_Shortest_Word_Distance.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class A243_Shortest_Word_Distance {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int index1 = -1, index2 = -1, min = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                if (index2 >= 0) {
+                    min = Math.min(min, i - index2);
+                }
+                index1 = i;
+            }
+            else if (words[i].equals(word2)) {
+                if (index1 >= 0) {
+                    min = Math.min(min, i - index1);
+                }
+                index2 = i;
+            }
+        }
+        return min;
+    }
+}
