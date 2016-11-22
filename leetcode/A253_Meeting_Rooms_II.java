5baee12b6b07c90624bc3fd537945b7aba4f7585
shawnchris <shawn.chris.gao@outlook.com>
2016-11-22 09:06:20 -0600

    253

---
+++ leetcode/A253_Meeting_Rooms_II.java
@@ -0,0 +1,68 @@
+package leetcode;
+import java.util.*;
+
+public class A253_Meeting_Rooms_II {
+	public int minMeetingRooms(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) return 0;
+        
+        Arrays.sort(intervals, new Comparator<Interval>() {
+           public int compare(Interval i1, Interval i2) {
+               return i1.start - i2.start;
+           }
+        });
+        
+        PriorityQueue<Interval> queue = new PriorityQueue<>(new Comparator<Interval>(){
+            public int compare(Interval i1, Interval i2) {
+                return i1.end - i2.end;
+            }
+        });
+        int max = 1;
+        for (int i = 0; i < intervals.length; i++) {
+            if (queue.isEmpty()) {
+                queue.add(intervals[i]);
+            }
+            else {
+                // Remove non-overlapping intervals on the top
+                while (!queue.isEmpty() && queue.peek().end <= intervals[i].start) {
+                    queue.poll();
+                }
+                // Now either queue is empty or all remaining intervals overlap with each other
+                // Prove: the smallest end is bigger than intervals[i].start, and all starts are no bigger than intervals[i].start
+                queue.add(intervals[i]);
+                max = Math.max(max, queue.size());
+            }
+        }
+        
+        return max;
+    }
+	
+	public int minMeetingRooms2(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) return 0;
+        
+        Arrays.sort(intervals, new Comparator<Interval>() {
+           public int compare(Interval i1, Interval i2) {
+               return i1.start - i2.start;
+           }
+        });
+        
+        PriorityQueue<Interval> queue = new PriorityQueue<>(new Comparator<Interval>(){
+            public int compare(Interval i1, Interval i2) {
+                return i1.end - i2.end;
+            }
+        });
+        
+        queue.add(intervals[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            Interval itv = queue.poll();
+            if (itv.end <= intervals[i].start) {
+            	itv.end = intervals[i].end;
+            }
+            else {
+            	queue.add(intervals[i]);
+            }
+            queue.add(itv);
+        }
+        
+        return queue.size();
+    }
+}
