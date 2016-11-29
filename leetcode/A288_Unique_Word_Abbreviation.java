57b0c22a8253a54eff3057124085933382a11926
shawnchris <shawn.chris.gao@outlook.com>
2016-11-29 09:17:05 -0600

    288

---
+++ leetcode/A288_Unique_Word_Abbreviation.java
@@ -0,0 +1,42 @@
+package leetcode;
+import java.util.*;
+
+public class A288_Unique_Word_Abbreviation {
+	public class ValidWordAbbr {
+	    Map<String, String> map;
+	    public ValidWordAbbr(String[] dictionary) {
+	        map = new HashMap<String, String>();
+	        for (String word : dictionary) {
+	            String abb = getAbbreviation(word);
+	            if (map.containsKey(abb)) {
+	                if (!map.get(abb).equals(word)) { // duplicate
+	                    map.put(abb, "");
+	                }
+	            }
+	            else {
+	                map.put(abb, word);
+	            }
+	        }
+	    }
+
+	    public boolean isUnique(String word) {
+	        String abb = getAbbreviation(word);
+	        return !map.containsKey(abb) || map.get(abb).equals(word);
+	    }
+	    
+	    private String getAbbreviation(String word) {
+	        if (word.length() < 3) return word;
+	        return word.substring(0, 1) + word.substring(1, word.length() - 1).length()
+	        		+ word.substring(word.length() - 1);
+	    }
+	}
+	public static void main(String[] args) {
+		A288_Unique_Word_Abbreviation uwa = new A288_Unique_Word_Abbreviation();
+		//["igprlgmj","xyitppjcakbxzsgntet"]
+		String[] dict = {"pelnlwwkfa","ayyrq","may","zzzg","akvzveklsk","pfziulaweuudrr","jccd","jrvzwkufpmze","cwfvhh","elmjdcysa","tdokjqfvrfjbwb","silsrnnvold","vqh","qyqnua","emwydo","aiehnrev","mplvtbipycf","cyxyde","ldyr","gtuvflak","xahpmzaj","pnb","wjneqv","hjh","crqw","uscswjlgemxqpp","mpnwvjgfrpzxjyxtasp","uwayurfgopyogumfm","ncbmyrbfejger","lozobnm","mwzfsezjkfvuumh","wxhkgtlqfvdto"};
+		ValidWordAbbr vw = uwa.new ValidWordAbbr(dict);
+		System.out.println(vw.isUnique("igprlgmj"));
+		System.out.println(vw.isUnique("xyitppjcakbxzsgntet"));
+	}
+
+}
