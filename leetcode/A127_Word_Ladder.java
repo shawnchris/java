bcaf4082dacde830af05b5511b74c4e5119e743d
shawnchris <shawn.chris.gao@outlook.com>
2016-12-04 19:08:45 -0600

    127

--- leetcode/A127_Word_Ladder.java
+++ leetcode/A127_Word_Ladder.java
@@ -2,6 +2,58 @@ package leetcode;
 import java.util.*;
 
 public class A127_Word_Ladder {
+	static class Node {
+        String word;
+        Node parent;
+        public Node(String w) { this.word = w; parent = null; }
+    }
+    public static int ladderLength2(String beginWord, String endWord, Set<String> wordList) {
+        if (beginWord.equals(endWord)) return 1;
+        
+        Set<String> visited = new HashSet<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(beginWord));
+        visited.add(beginWord);
+        
+        boolean found = false;
+        int level = 1;
+        while (!queue.isEmpty() && !found) {
+            int size = queue.size();
+            for (int i = 0; i < size && !found; i++) {
+                Node node = queue.poll();
+                for (int j = 0; j < node.word.length() && !found; j++) {
+                	char[] array = node.word.toCharArray();
+                    for (int k = 0; k < 26; k++) {
+                        array[j] = (char)('a' + k);
+                        String next = new String(array);
+                        if (next.equals(endWord)) {
+                            printPath(node);
+                            System.out.println(next);
+                            found = true;
+                            break;
+                        }
+                        if (wordList.contains(next) && !visited.contains(next)) {
+                            Node newNode = new Node(next);
+                            newNode.parent = node;
+                            queue.add(newNode);
+                            visited.add(next);
+                        }
+                    }
+                }
+            }
+            level++;
+        }
+        
+        if (found) return level;
+        return 0;
+    }
+    
+    private static void printPath(Node node) {
+        if (node.parent != null) printPath(node.parent);
+        System.out.print(node.word + "->");
+    }
+    
+    
 	public static int ladderLength(String beginWord, String endWord, Set<String> wordList) {
         if (beginWord.equals(endWord)) return 0;
 
@@ -44,6 +96,7 @@ public class A127_Word_Ladder {
 		String endWord = "cog";
 		Set<String> wordList = new HashSet<>(Arrays.asList("hot","dot","dog","lot","log"));
 		System.out.println(ladderLength(beginWord, endWord, wordList));
+		System.out.println(ladderLength2(beginWord, endWord, wordList));
 	}
 
 }
