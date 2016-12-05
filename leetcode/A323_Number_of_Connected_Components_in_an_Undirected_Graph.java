692028f8ed9b35feb079f3038582c7c15ac73d73
shawnchris <shawn.chris.gao@outlook.com>
2016-12-04 19:08:56 -0600

    323

---
+++ leetcode/A323_Number_of_Connected_Components_in_an_Undirected_Graph.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+public class A323_Number_of_Connected_Components_in_an_Undirected_Graph {
+    class UnionFind {
+        private int count = 0;
+        private int[] parent, rank;
+        
+        public UnionFind(int n) {
+            count = n;
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+        
+        public int find(int p) {
+            int q = parent[p];
+            while (q != parent[q]) {
+                q = parent[q];
+            }
+            parent[p] = q;
+            return q;
+        }
+        
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP == rootQ) return;
+            if (rank[rootQ] > rank[rootP]) {
+                parent[rootP] = rootQ;
+            }
+            else {
+                parent[rootQ] = rootP;
+                if (rank[rootP] == rank[rootQ]) {
+                    rank[rootP]++;
+                }
+            }
+            count--;
+        }
+        
+        public int count() {
+            return count;
+        }
+    }
+    
+    public int countComponents(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n);
+        for (int[] edge : edges) {
+            uf.union(edge[0], edge[1]);
+        }
+        return uf.count();
+    }
+}
