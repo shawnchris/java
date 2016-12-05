5650a5df5cab3be7387d2bebd4260df29ca686ce
shawnchris <shawn.chris.gao@outlook.com>
2016-12-04 19:08:40 -0600

    47

--- leetcode/A047_PermutationsII.java
+++ leetcode/A047_PermutationsII.java
@@ -2,6 +2,35 @@ package leetcode;
 import java.util.*;
 
 public class A047_PermutationsII {
+	public List<List<Integer>> permuteUnique2(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length == 0) return result;
+        
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        helper(nums, nums.length, new ArrayList<Integer>(), used, result);
+        return result;
+    }
+    
+    private void helper(int[] nums, int left, List<Integer> current, boolean[] used, 
+                            List<List<Integer>> result) {
+        if (left == 0) {
+            result.add(new ArrayList<Integer>(current));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            if (i != 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+            used[i] = true;
+            current.add(nums[i]);
+            helper(nums, left - 1, current, used, result);
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+	
+	
     List<List<Integer>> result = null;
     
     private void permuteHelper(int[] nums, int left, List<Integer> curResult, Set<Integer> used) {
