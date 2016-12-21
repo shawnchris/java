ddc81ae287942e6201a374d30996d72fa9d40bf4
shawnchris <shawn.chris.gao@outlook.com>
2016-12-21 10:12:46 -0600

    425

--- leetcode/A425_Word_Squares.java
+++ leetcode/A425_Word_Squares.java
@@ -78,4 +78,58 @@ public class A425_Word_Squares {
             ansBuilder.remove(ansBuilder.size() - 1);
         }
     }
+    
+    public List<List<String>> wordSquares2(String[] words) {
+    	List<List<String>> result = new ArrayList<List<String>>();
+    	if (words == null || words.length == 0) return result;
+    	
+    	int len = words[0].length();
+    	Map<String, List<String>> map = new HashMap<>();
+    	for (String word : words) {
+    		for (int i = 1; i <= len; i++) {
+    			String key = word.substring(0, i);
+    			List<String> list = map.get(key);
+    			if (list == null) list = new ArrayList<String>();
+    			list.add(word);
+    			map.put(key, list);
+    		}
+    	}
+    	
+    	for (String word : words) {
+    		List<String> current = new ArrayList<>();
+    		current.add(word);
+    		dfs(len, 1, current, map, result);
+    	}
+    	
+    	return result;
+    }
+    
+    private void dfs(int len, int level, List<String> current, Map<String, List<String>> map, List<List<String>> result) {
+    	if (len == level) {
+    		result.add(new ArrayList<String>(current));
+    		return;
+    	}
+    	
+    	String prefix = "";
+    	for (String s : current) {
+    		prefix = prefix + s.charAt(level);
+    	}
+    	List<String> candidates = map.get(prefix);
+    	if (candidates == null) return;
+    	for (String s : candidates) {
+    		current.add(s);
+    		dfs(len, level + 1, current, map, result);
+    		current.remove(current.size() - 1);
+    	}
+    }
+    
+    public static void main(String[] args) {
+    	A425_Word_Squares ws = new A425_Word_Squares();
+    	String[] words1 = {"area","lead","wall","lady","ball"};
+    	String[] words2 = {"abat","baba","atan","atal"};
+    	System.out.println(ws.wordSquares(words1));
+    	System.out.println(ws.wordSquares(words2));
+    	System.out.println(ws.wordSquares2(words1));
+    	System.out.println(ws.wordSquares2(words2));
+    }
 }
