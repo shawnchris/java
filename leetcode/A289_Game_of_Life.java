9a78b15d3416a3dda274d54125b80a803ddd06ea
shawnchris <shawn.chris.gao@outlook.com>
2017-01-13 11:59:41 -0600

    289

--- leetcode/A289_Game_of_Life.java
+++ leetcode/A289_Game_of_Life.java
@@ -2,6 +2,44 @@ package leetcode;
 
 //https://leetcode.com/discuss/68352/easiest-java-solution-with-explanation
 public class A289_Game_of_Life {
+	public void gameOfLife2(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                int count = countLife(board, i, j);
+                if (board[i][j] == 1) { //live cell
+                    if (count >= 2 && count <= 3) { //going to live
+                        board[i][j] = 3;
+                    }
+                    //otherwise going to die
+                }
+                else { //dead cell
+                    if (count == 3) { //going to live
+                        board[i][j] = 2;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] >>= 1;
+            }
+        }
+    }
+    
+    private int countLife(int[][] board, int row, int col) {
+        int result = 0;
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                int r = row + i, c = col + j;
+                if (r < 0 || r >= board.length || c < 0 || c >= board[0].length
+                    || (r == row && c == col)) continue;
+                if ((board[r][c] & 1) == 1) result++;
+            }
+        }
+        return result;
+    }
+	
     public void gameOfLife(int[][] board) {
         if(board == null || board.length == 0) return;
         int m = board.length, n = board[0].length;
