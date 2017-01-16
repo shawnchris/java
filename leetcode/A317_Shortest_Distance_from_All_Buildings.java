f6a5668e45e903ca30401462adb9910ad94900c8
shawnchris <shawn.chris.gao@outlook.com>
2017-01-16 13:27:54 -0600

    317

--- leetcode/A317_Shortest_Distance_from_All_Buildings.java
+++ leetcode/A317_Shortest_Distance_from_All_Buildings.java
@@ -1,63 +1,74 @@
 package leetcode;
 import java.util.*;
 public class A317_Shortest_Distance_from_All_Buildings {
+	static int[] dr = {0, 0, -1, 1};
+    static int[] dc = {-1, 1, 0, 0};
+    
     public int shortestDistance(int[][] grid) {
-        if (grid == null || grid[0].length == 0) return 0;
-        final int[] shift = new int[] {0, 1, 0, -1, 0};
+        if (grid == null) return -1;
+        int m = grid.length;
+        if (m == 0) return -1;
+        int n = grid[0].length;
+        if (n == 0) return -1;
         
-        int row  = grid.length, col = grid[0].length;
-        int[][] distance = new int[row][col];
-        int[][] reach = new int[row][col];
-        int buildingNum = 0;
+        int[][] distance = new int[m][n];
+        int[][] reach = new int[m][n];
+        int numBuilding = 0, result = Integer.MAX_VALUE;
         
-        for (int i = 0; i < row; i++) {
-            for (int j =0; j < col; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    buildingNum++;
-                    Queue<int[]> myQueue = new LinkedList<int[]>();
-                    myQueue.offer(new int[] {i,j});
-
-                    boolean[][] isVisited = new boolean[row][col];
-                    int level = 1;
-                    
-                    while (!myQueue.isEmpty()) {
-                        int qSize = myQueue.size();
-                        for (int q = 0; q < qSize; q++) {
-                            int[] curr = myQueue.poll();
-                            
-                            for (int k = 0; k < 4; k++) {
-                                int nextRow = curr[0] + shift[k];
-                                int nextCol = curr[1] + shift[k + 1];
-                                
-                                if (nextRow >= 0 && nextRow < row && nextCol >= 0 && nextCol < col
-                                    && grid[nextRow][nextCol] == 0 && !isVisited[nextRow][nextCol]) {
-                                        //The shortest distance from [nextRow][nextCol] to thic building
-                                        // is 'level'.
-                                        distance[nextRow][nextCol] += level;
-                                        reach[nextRow][nextCol]++;
-                                        
-                                        isVisited[nextRow][nextCol] = true;
-                                        myQueue.offer(new int[] {nextRow, nextCol});
-                                    }
-                            }
-                        }
-                        level++;
-                    }
+                    numBuilding++;
+                    bfs(grid, distance, reach, i, j);
                 }
             }
         }
         
-        int shortest = Integer.MAX_VALUE;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 0 && reach[i][j] == buildingNum) {
-                    shortest = Math.min(shortest, distance[i][j]);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (reach[i][j] == numBuilding) {
+                    result = Math.min(result, distance[i][j]);
                 }
             }
         }
         
-        return shortest == Integer.MAX_VALUE ? -1 : shortest;
+        if (result == Integer.MAX_VALUE) return -1;
+        return result;
+    }
+    
+    private void bfs(int[][] grid, int[][] distance, int[][] reach, int row, int col) {
+        Set<Long> visited = new HashSet<>();
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {row, col, 0});
+        visited.add(((long)row << 31) | col);
         
-        
+        while (!queue.isEmpty()) {
+            int[] p = queue.poll();
+            int r1 = p[0];
+            int c1 = p[1];
+            for (int i = 0; i < 4; i++) {
+                int c2 = c1 + dc[i];
+                int r2 = r1 + dr[i];
+                if (c2 < 0 || c2 >= grid[0].length || r2 < 0 || r2 >= grid.length
+                    || grid[r2][c2] != 0) continue;
+                long q = ((long)r2 << 31) | c2;
+                if (visited.contains(q)) continue;
+                
+                reach[r2][c2]++;
+                distance[r2][c2] += p[2] + 1;
+                
+                queue.add(new int[] {r2, c2, p[2] + 1});
+                visited.add(q);
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+    	int[][] grid1 = {{1,0,2,0,1},
+    			{0,0,0,0,0},
+    			{0,0,1,0,0}};
+    	
+    	A317_Shortest_Distance_from_All_Buildings sd = new A317_Shortest_Distance_from_All_Buildings();
+    	System.out.println(sd.shortestDistance(grid1));
     }
 }
