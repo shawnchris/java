41720c797900ad7b1e9ec12991143b19411f8ef8
shawnchris <shawn.chris.gao@outlook.com>
2017-01-18 11:50:47 -0600

    67

--- leetcode/A067_Add_Binary.java
+++ leetcode/A067_Add_Binary.java
@@ -2,29 +2,25 @@ package leetcode;
 
 public class A067_Add_Binary {
     public String addBinary(String a, String b) {
-        if (a == null || a.length() == 0)
-            return b;
-        if (b == null || b.length() == 0)
-            return a;
-        char[] aArray = a.toCharArray();
-        char[] bArray = b.toCharArray();
-        String result = "";
-
-        int i = aArray.length - 1;
-        int j = bArray.length - 1;
-        int aByte = 0, bByte = 0, carry = 0, res = 0;
-
-        while(i > -1 || j > -1 || carry == 1) {
-            if (i > -1) aByte = aArray[i] - '0';
-            else aByte = 0;
-            if (j > -1) bByte = bArray[j] - '0';
-            else bByte = 0;
-            res = aByte ^ bByte ^ carry;
-            carry = ((aByte + bByte + carry) >= 2) ? 1 : 0;
-            result = res + result;
-            i--;
-            j--;
+    	if (a == null || a.length() == 0) return b;
+        if (b == null || b.length() == 0) return a;
+        
+        int i = a.length() - 1, j = b.length() - 1, sum = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        while (i >= 0 || j >= 0 || sum > 0) {
+            if (i >= 0) {
+                sum += Integer.parseInt(a.substring(i, i + 1));
+                i--;
+            }
+            if (j >= 0) {
+                sum += Integer.parseInt(b.substring(j, j + 1));
+                j--;
+            }
+            sb.append(sum % 2);
+            sum = sum / 2;
         }
-        return result;
+        
+        return sb.reverse().toString();
     }
 }
