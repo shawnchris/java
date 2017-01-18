54798a123a29ffb1bf92166717364beab08f4757
shawnchris <shawn.chris.gao@outlook.com>
2017-01-18 11:37:59 -0600

    314

--- leetcode/A314_Binary_Tree_Vertical_Order_Traversal.java
+++ leetcode/A314_Binary_Tree_Vertical_Order_Traversal.java
@@ -3,40 +3,38 @@ import java.util.*;
 
 public class A314_Binary_Tree_Vertical_Order_Traversal {
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
+    	List<List<Integer>> result = new ArrayList<List<Integer>>();
         if (root == null) return result;
         
         Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
         Queue<TreeNode> qNode = new LinkedList<>();
-        Queue<Integer> qLevel = new LinkedList<>();
+        Queue<Integer> qIndex = new LinkedList<>();
         qNode.add(root);
-        qLevel.add(0);
+        qIndex.add(0);
         
-        // BFS
         while (!qNode.isEmpty()) {
             TreeNode node = qNode.poll();
-            int level = qLevel.poll();
-            List<Integer> list = map.get(level);
-            if (list == null) {
-                list = new ArrayList<Integer>();
-            }
+            int index = qIndex.poll();
+            
+            List<Integer> list = map.get(index);
+            if (list == null) list = new ArrayList<Integer>();
             list.add(node.val);
-            map.put(level, list);
+            map.put(index, list);
             
             if (node.left != null) {
                 qNode.add(node.left);
-                qLevel.add(level - 1);
+                qIndex.add(index - 1);
             }
             if (node.right != null) {
                 qNode.add(node.right);
-                qLevel.add(level + 1);
+                qIndex.add(index + 1);
             }
         }
         
-        Integer[] levels = map.keySet().toArray(new Integer[map.keySet().size()]);
-        Arrays.sort(levels);
-        for (int level : levels) {
-            result.add(map.get(level));
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+        for (int key : keys) {
+            result.add(map.get(key));
         }
         
         return result;
