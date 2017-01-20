1153fb242ff08fc6ec132f8f3a55ef19e92d0941
shawnchris <shawn.chris.gao@outlook.com>
2017-01-19 22:04:58 -0600

    17

--- leetcode/A017_Letter_Combinations_of_a_Phone_Number.java
+++ leetcode/A017_Letter_Combinations_of_a_Phone_Number.java
@@ -2,30 +2,27 @@ package leetcode;
 import java.util.*;
 
 public class A017_Letter_Combinations_of_a_Phone_Number {
-    private void helper(String digits, String current, int level, List<String> result, String[] map) {
-        if (level == digits.length()) {
-            result.add(current);
+    private static String[] dict = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.length() == 0) return result;
+        
+        helper(result, digits.toCharArray(), new char[digits.length()], 0);
+        return result;
+    }
+    
+    private void helper(List<String> result, char[] digits, char[] current, int index) {
+        if (index == digits.length) {
+            result.add(new String(current));
             return;
         }
         
-        int c = digits.charAt(level) - '0';
-        for (int i = 0; i < map[c].length(); i++) {
-            helper(digits, current + map[c].substring(i, i + 1), level + 1, result, map);
+        String word = dict[digits[index] - '0'];
+        for (int i = 0; i < word.length(); i++) {
+            current[index] = word.charAt(i);
+            helper(result, digits, current, index + 1);
         }
-    }
-    
-    public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<String>();
-        if (digits==null) return null;
-        int len = digits.length();
-        if (len==0) return result;
-        
-		String[] map = new String[]
-				{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-		
-		helper(digits, "", 0, result, map);
-		
-		return result;
     }
     
 }
