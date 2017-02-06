e76370d1dea6e3bf3675c5efc981ce42fbc7da7f
shawnchris <shawn.chris.gao@outlook.com>
2017-02-05 22:07:34 -0600

    508

---
+++ leetcode/A508_Most_Frequent_Subtree_Sum.java
@@ -0,0 +1,42 @@
+package leetcode;
+import java.util.*;
+
+public class A508_Most_Frequent_Subtree_Sum {
+    Map<Integer, Integer> sumToCount;
+    int maxCount;
+    
+    public int[] findFrequentTreeSum(TreeNode root) {
+        maxCount = 0;
+        sumToCount = new HashMap<Integer, Integer>();
+        
+        postOrder(root);
+        
+        List<Integer> res = new ArrayList<>();
+        for (int key : sumToCount.keySet()) {
+            if (sumToCount.get(key) == maxCount) {
+                res.add(key);
+            }
+        }
+        
+        int[] result = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+    
+    private int postOrder(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = postOrder(root.left);
+        int right = postOrder(root.right);
+        
+        int sum = left + right + root.val;
+        int count = sumToCount.getOrDefault(sum, 0) + 1;
+        sumToCount.put(sum, count);
+        
+        maxCount = Math.max(maxCount, count);
+        
+        return sum;
+    }
+}
