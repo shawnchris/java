a618cf1859c4382bd410c80792cd47c630fc7e5a
shawnchris <shawn.chris.gao@outlook.com>
2017-02-11 10:04:30 -0600

    hash map

--- datastructure/MyHashMap.java
+++ datastructure/MyHashMap.java
@@ -1,7 +1,25 @@
 package datastructure;
-
-public class MyHashMap {
-
+/*
+ * 1. Open spacing
+ * 2. Separate chaining
+ */
+public class MyHashMap<K, V> {
+	public void put(K key, V value) {
+		
+	}
+	
+	public V get(K key) {
+		return null;
+	}
+	
+	public V remove(K key) {
+		return null;
+	}
+	
+	public boolean containsKey(K key) {
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
