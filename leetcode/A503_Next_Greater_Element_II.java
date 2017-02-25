70f30f9ac50ff3e349074003b10a016a074e9017
shawnchris <shawn.chris.gao@outlook.com>
2017-02-24 19:18:37 -0600

    503

---
+++ leetcode/A503_Next_Greater_Element_II.java
@@ -0,0 +1,27 @@
+package leetcode;
+import java.util.*;
+
+public class A503_Next_Greater_Element_II {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        Stack<Integer> stack = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            stack.push(i);
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = -1;
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()){
+                result[i] = nums[stack.peek()];
+            }
+            stack.add(i);
+        }
+        
+        return result;
+    }
+}
