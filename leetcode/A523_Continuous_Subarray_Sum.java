2a6243d33597a15cea30f2962c5372c785af8f44
Shawn Gao <shawn.chris.gao@outlook.com>
2017-03-07 15:19:11 -0600

    523

---
+++ leetcode/A523_Continuous_Subarray_Sum.java
@@ -0,0 +1,19 @@
+package leetcode;
+import java.util.*;
+
+public class A523_Continuous_Subarray_Sum {
+	public boolean checkSubarraySum(int[] nums, int k) {
+	    Map<Integer, Integer> map = new HashMap<Integer, Integer>(){{put(0,-1);}};;
+	    int runningSum = 0;
+	    for (int i=0;i<nums.length;i++) {
+	        runningSum += nums[i];
+	        if (k != 0) runningSum %= k; 
+	        Integer prev = map.get(runningSum);
+	        if (prev != null) {
+	            if (i - prev > 1) return true;
+	        }
+	        else map.put(runningSum, i);
+	    }
+	    return false;
+	}
+}
