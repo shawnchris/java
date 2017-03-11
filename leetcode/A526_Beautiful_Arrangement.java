0bdf6807abb466a652abdae5bf4b9d3d62a4face
Shawn Gao <shawn.chris.gao@outlook.com>
2017-03-11 15:21:02 -0600

    526

---
+++ leetcode/A526_Beautiful_Arrangement.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class A526_Beautiful_Arrangement {
+    int count = 0;
+    
+    public int countArrangement(int N) {
+        if (N == 0) return 0;
+        helper(N, 1, new int[N + 1]);
+        return count;
+    }
+    
+    private void helper(int N, int pos, int[] used) {
+        if (pos > N) {
+            count++;
+            return;
+        }
+        
+        for (int i = 1; i <= N; i++) {
+            if (used[i] == 0 && (i % pos == 0 || pos % i == 0)) {
+                used[i] = 1;
+                helper(N, pos + 1, used);
+                used[i] = 0;
+            }
+        }
+    }
+}
