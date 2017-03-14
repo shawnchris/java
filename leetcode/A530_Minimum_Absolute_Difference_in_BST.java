3737283a6b3fb38810029250c0221ae6ab328f02
Shawn Gao <shawn.chris.gao@outlook.com>
2017-03-14 15:21:51 -0500

    530

---
+++ leetcode/A530_Minimum_Absolute_Difference_in_BST.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class A530_Minimum_Absolute_Difference_in_BST {
+    int min = Integer.MAX_VALUE;
+    Integer prev = null;
+    
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) return min;
+        
+        getMinimumDifference(root.left);
+        
+        if (prev != null) {
+            min = Math.min(min, root.val - prev);
+        }
+        prev = root.val;
+        
+        getMinimumDifference(root.right);
+        
+        return min;
+    }
+}
