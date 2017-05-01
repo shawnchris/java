1090d7832047da2745738ac4836681cb1569a515
Shawn Gao <shawn.chris.gao@outlook.com>
2017-05-01 08:51:56 -0500

    557

---
+++ leetcode/A557_Reverse_Words_in_a_String_III.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class A557_Reverse_Words_in_a_String_III {
+    public String reverseWords(String s) {
+        char[] ca = s.toCharArray();
+        for (int i = 0; i < ca.length; i++) {
+            if (ca[i] != ' ') {   // when i is a non-space
+                int j = i;
+                while (j + 1 < ca.length && ca[j + 1] != ' ') { j++; } // move j to the end of the word
+                reverse(ca, i, j);
+                i = j;
+            }
+        }
+        return new String(ca);
+    }
+
+    private void reverse(char[] ca, int i, int j) {
+        for (; i < j; i++, j--) {
+            char tmp = ca[i];
+            ca[i] = ca[j];
+            ca[j] = tmp;
+        }
+    }
+}
