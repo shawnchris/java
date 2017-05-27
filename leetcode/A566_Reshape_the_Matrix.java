80becd659b04dcbf420aab4f18e9d57f9e0b9ed5
shawnchris <shawn.chris.gao@outlook.com>
2017-05-27 15:56:41 -0700

    562

---
+++ leetcode/A566_Reshape_the_Matrix.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class A566_Reshape_the_Matrix {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int m = nums.length, n = nums[0].length;
+        if (m * n != r * c) return nums;
+
+        int[][] result = new int[r][c];
+        int row = 0, col = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[row][col] = nums[i][j];
+                col++;
+                if (col == c) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
