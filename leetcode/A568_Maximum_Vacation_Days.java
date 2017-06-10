b4f80a2d14c630dc55dfa33009fe6f4f8bc9d90d
shawnchris <shawn.chris.gao@outlook.com>
2017-06-10 15:57:25 -0700

    562

---
+++ leetcode/A568_Maximum_Vacation_Days.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class A568_Maximum_Vacation_Days {
+    public int maxVacationDays(int[][] flights, int[][] days) {
+        int N = flights.length;
+        int K = days[0].length;
+        int[] dp = new int[N];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+        dp[0] = 0;
+
+        for (int i = 0; i < K; i++) {
+            int[] temp = new int[N];
+            Arrays.fill(temp, Integer.MIN_VALUE);
+            for (int j = 0; j < N; j++) {
+                for(int k = 0; k < N; k++) {
+                    if (j == k || flights[k][j] == 1) {
+                        temp[j] = Math.max(temp[j], dp[k] + days[j][i]);
+                    }
+                }
+            }
+            dp = temp;
+        }
+
+        int max = 0;
+        for (int v : dp) {
+            max = Math.max(max, v);
+        }
+
+        return max;
+    }
+}
