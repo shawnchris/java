b6c5b3a8956ef503bfc3d35e1e0ccaff81cad87c
shawnchris <shawn.chris.gao@outlook.com>
2017-07-01 23:38:28 -0700

    -

---
+++ leetcode/A572_Subtree_of_Another_Tree.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class A572_Subtree_of_Another_Tree {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+
+        if (s.val != t.val) return false;
+
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+}
