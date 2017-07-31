1bd7aa0277c4b62733fda38757e0b6e9cd4bf8e5
shawnchris <shawn.chris.gao@outlook.com>
2017-07-31 11:32:26 -0700

    Add new problems

---
+++ leetcode/A611_Valid_Triangle_Number.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class A611_Valid_Triangle_Number {
+    public int triangleNumber(int[] nums) {
+        int result = 0;
+        if (nums.length < 3) return result;
+
+        Arrays.sort(nums);
+
+        for (int i = 2; i < nums.length; i++) {
+            int left = 0, right = i - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] > nums[i]) {
+                    result += (right - left);
+                    right--;
+                }
+                else {
+                    left++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
