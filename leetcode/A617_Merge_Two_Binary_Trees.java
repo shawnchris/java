5abe3d59ec5d74c0845200aa5405043fdd11858f
shawnchris <shawn.chris.gao@outlook.com>
2017-08-05 11:33:00 -0700

    Add new problems

---
+++ leetcode/A617_Merge_Two_Binary_Trees.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class A617_Merge_Two_Binary_Trees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+
+        int val = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val);
+        TreeNode newNode = new TreeNode(val);
+
+        newNode.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
+        newNode.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+
+        return newNode;
+    }
+}
