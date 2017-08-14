e51a8a9850f7f545ff7f8bd9957c89ad07fe4faa
shawnchris <shawn.chris.gao@outlook.com>
2017-08-14 12:12:27 -0700

    Add new problems

---
+++ leetcode/A624_Maximum_Distance_in_Arrays.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.List;
+
+public class A624_Maximum_Distance_in_Arrays {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int result = Integer.MIN_VALUE;
+        int max = arrays.get(0).get(arrays.get(0).size() - 1);
+        int min = arrays.get(0).get(0);
+
+        for (int i = 1; i < arrays.size(); i++) {
+            result = Math.max(result, Math.abs(arrays.get(i).get(0) - max));
+            result = Math.max(result, Math.abs(arrays.get(i).get(arrays.get(i).size() - 1) - min));
+            max = Math.max(max, arrays.get(i).get(arrays.get(i).size() - 1));
+            min = Math.min(min, arrays.get(i).get(0));
+        }
+
+        return result;
+    }
+}
