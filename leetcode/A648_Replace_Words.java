6f556914d9d2214043b8cb196143a5b71a11be96
shawnchris <shawn.chris.gao@outlook.com>
2017-09-14 16:57:29 -0700

    Add new problems

---
+++ leetcode/A648_Replace_Words.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class A648_Replace_Words {
+    public String replaceWords(List<String> dict, String sentence) {
+        if (dict == null || dict.size() == 0) return sentence;
+
+        Set<String> set = new HashSet<>();
+        for (String s : dict) set.add(s);
+
+        StringBuilder sb = new StringBuilder();
+        String[] words = sentence.split("\\s+");
+
+        for (String word : words) {
+            String prefix = "";
+            for (int i = 1; i <= word.length(); i++) {
+                prefix = word.substring(0, i);
+                if (set.contains(prefix)) break;
+            }
+            sb.append(" " + prefix);
+        }
+
+        return sb.deleteCharAt(0).toString();
+    }
+}
