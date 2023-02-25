f2b9020cb49df55514d511049edc80d907a60afc
shawngao <shawngao@outlook.com>
2023-02-24 19:45:59 -0800

    HackerRank

--- leetcode/A664_Strange_Printer.java
+++ leetcode/A664_Strange_Printer.java
@@ -1,20 +1,31 @@
 package leetcode;
 
 public class A664_Strange_Printer {
+    // We can further divide the substring to two parts: start -> start+k and start+k+1 -> start+len.
+    // It is something as following:
+    // index |start  ...  start + k| |start + k + 1 ... start + len|
+    // char  |  a    ...       b   | |      c       ...      b     |
+    // If we have s.charAt(start + k) == s.charAt(start + len), we can reduce one turn.
     public int strangePrinter(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
         int n = s.length();
-        if (n == 0) return 0;
+        int[][] dp = new int[n][n];
 
-        int[][] dp = new int[101][101];
-        for (int i = 0; i < n; i++) dp[i][i] = 1;
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < n - i; j++) {
-                dp[j][j + i] = i + 1;
-                for (int k = j + 1; k <= j + i; k++) {
-                    int temp = dp[j][k - 1] + dp[k][j + i];
-                    if (s.charAt(k - 1) == s.charAt(j + i)) temp--;
-                    dp[j][j + i] = Math.min(dp[j][j + i], temp);
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int len = 1; len < n; len++) {
+            for (int left = 0; left < n - len; left++) {
+                int right = left + len;
+                dp[left][right] = len + 1;
+                for (int mid = left; mid < right; mid++) {
+                    int steps = dp[left][mid] + dp[mid + 1][right];
+                    if (s.charAt(mid) == s.charAt(right)) {
+                        steps--;
+                    }
+                    dp[left][right] = Math.min(steps, dp[left][right]);
                 }
             }
         }
